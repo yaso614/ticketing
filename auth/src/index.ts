096a7dfef61103d7ede4237ddf5d59a59bc57a45
yaso614 <kebinkastora@naver.com>
2020-06-09 17:37:41 +0900

    king king

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import { app } from './app';
 
 const start = async () => {
-  console.log('No this is king2');
+  console.log('No this is king 3');
 
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
